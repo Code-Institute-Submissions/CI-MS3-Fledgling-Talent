66f3430ba5fe73c362ac6b4366374602943d1365
scottsimpson91 <scottsimpson7@googlemail.com>
2021-02-04 11:55:30 +0000

    FEAT: Add sign in functionality and styles

--- app.py
+++ app.py
@@ -49,6 +49,31 @@ def register():
     return render_template("register.html")
 
 
+@app.route("/sign_in", methods=["GET", "POST"])
+def sign_in():
+    if request.method == "POST":
+        # Check if username exists in DB
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # Ensure hashed password matches user input
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                    session["user"] = request.form.get("username").lower()
+                    flash("Welcome, {}".format(request.form.get("username")))
+            else:
+                # Invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("sign_in"))
+
+        else:
+            # Username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("sign_in"))
+    return render_template("sign_in.html")
+
+
 # Find Jobs Page
 @app.route("/get_jobs")
 def get_jobs():
